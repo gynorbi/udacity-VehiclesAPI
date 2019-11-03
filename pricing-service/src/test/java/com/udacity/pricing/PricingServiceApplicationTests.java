f84ebbda83d2bede56de7e43a001ec5415a3ee0f
gynorbi <gynorbi@gmail.com>
2019-11-03 20:28:06 +0200

    Added one more unit test.

--- pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,7 +1,10 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.domain.price.PriceRepository;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class PricingServiceApplicationTests {
 
+	@Autowired
+	private PriceRepository priceRepository;
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void autoInitOfRepository(){
+		Assert.assertTrue("Repository should have been initialized",priceRepository.count()>0);
 	}
 
 }
